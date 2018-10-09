658d509bbbd757516749913acb86d5db8635c736
Itmad <315096953@qq.com>
2018-10-10 00:29:38 +0800

    更新下数据库
    Signed-off-by: Itmad <315096953@qq.com>

--- t_pay.sql
+++ t_pay.sql
@@ -2,14 +2,13 @@
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS `pay_black`;
 CREATE TABLE `pay_black`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip` char(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '要加黑名单的IP值',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '黑名单列表，以IP为限制，防止部分人恶意攻击，暂时不加此功能' ROW_FORMAT = Fixed;
 
-DROP TABLE IF EXISTS `pay_log`;
+
 CREATE TABLE `pay_log`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '日志内容主体',
@@ -20,7 +19,6 @@ CREATE TABLE `pay_log`  (
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '日志记录表，也没多少好说的。。' ROW_FORMAT = Dynamic;
 
 
-DROP TABLE IF EXISTS `pay_police`;
 CREATE TABLE `pay_police`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '对方ip值',
@@ -32,14 +30,12 @@ CREATE TABLE `pay_police`  (
 ) ENGINE = MyISAM AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '这个表每小时会清空一次' ROW_FORMAT = Fixed;
 
 
-DROP TABLE IF EXISTS `pay_task`;
 CREATE TABLE `pay_task`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `money` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '价格，单位为分',
   `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '二维码的字符串',
   `channel` char(8) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '支付渠道名,wechat或alipay',
   `task_extra` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '此订单的附加值，可传激活码和用户名等等，自己分割',
-  `result_extra` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '客户端来询问是否充值成功时，将返回这个值',
   `mark_sell` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '收款方备注',
   `mark_buy` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '付款方备注',
   `order_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '订单编号',
@@ -53,7 +49,7 @@ CREATE TABLE `pay_task`  (
   UNIQUE INDEX `mianindex`(`mark_sell`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '支付任务的主要表' ROW_FORMAT = Dynamic;
 
-DROP TABLE IF EXISTS `test_tab_user`;
+
 CREATE TABLE `test_tab_user`  (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user` char(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
@@ -61,5 +57,6 @@ CREATE TABLE `test_tab_user`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `user_index`(`user`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '你们可以照葫芦画瓢用的测试表' ROW_FORMAT = Fixed;
+
 
 SET FOREIGN_KEY_CHECKS = 1;
